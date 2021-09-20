04a556fdaf21654ea949f83e8f56e76646a5e4ad
Ville Skyttä <ville.skytta@iki.fi>
2021-09-20 22:54:12 +0300

    refactor: switch to src directory layout
    https://blog.ionelmc.ro/2014/05/25/python-packaging/#the-structure

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup  # type: ignore[import]
 
 def get_version() -> str:
     """Extract version number."""
-    fname = "pytekukko/version.py"
+    fname = "src/pytekukko/version.py"
     with open(fname, encoding="utf-8") as file_:
         for line in file_:
             if line.startswith("__version__"):
