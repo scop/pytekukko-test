2486c93865d361879f60db33a3473ed5881e5cf5
Ville Skyttä <ville.skytta@iki.fi>
2021-12-05 00:05:11 +0200

    chore(setup): spelling fixes

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from setuptools import setup  # type: ignore[import]
 
 def get_version() -> str:
     """Extract version number."""
-    fname = "src/pytekukko/__init__.py"
-    with open(fname, encoding="utf-8") as file_:
+    filename = "src/pytekukko/__init__.py"
+    with open(filename, encoding="utf-8") as file_:
         for line in file_:
             if line.startswith("__version__"):
                 return line.split("=")[-1].strip("\"'\r\n ")
-    raise Exception(f"No __version__ in {fname}!")
+    raise Exception(f"No __version__ in {filename}!")
 
 
 if __name__ == "__main__":
