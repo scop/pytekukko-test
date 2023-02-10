5d95763f2df25dbf2e1b202df31c797cec85f310
Ville Skyttä <ville.skytta@iki.fi>
2023-02-10 23:23:15 +0200

    test: skip aiohttp, frozenlist, and yarl native builds with Python 3.12
    They don't compile at the moment, as of ~3.12.0a5.

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,7 @@
 # noqa: INP001
 """nox config for pytekukko."""
 
+from typing import cast
 
 import nox
 
@@ -10,6 +11,12 @@ nox.options.error_on_external_run = True
 @nox.session(python=[f"{py}3.{x}" for py in ("", "pypy") for x in range(9, 13)])
 def test(session: nox.Session) -> None:
     """Run tests."""
+    if int(cast(str, session.python).rpartition(".")[2]) >= 12:  # noqa: PLR2004
+        session.env.update(
+            AIOHTTP_NO_EXTENSIONS="1",
+            FROZENLIST_NO_EXTENSIONS="1",
+            YARL_NO_EXTENSIONS="1",
+        )
     session.install(".[examples]", "-r", "requirements-test.txt")
 
     prefix = "python3 -X dev -bb"
