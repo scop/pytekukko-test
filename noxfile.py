5159f126e3b3e172acedb026bc6f86377600a1b0
Ville Skyttä <ville.skytta@iki.fi>
2024-11-20 00:05:12 +0200

    test: remove no longer needed Python >= 3.13 build workarounds

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,5 @@
 """nox config for pytekukko."""
 
-from typing import cast
-
 import nox
 
 nox.options.error_on_external_run = True
@@ -10,11 +8,6 @@ nox.options.error_on_external_run = True
 @nox.session(python=[f"{py}3.{x}" for py in ("", "pypy") for x in range(10, 15)])
 def test(session: nox.Session) -> None:
     """Run tests."""
-    py_minor = int(cast(str, session.python).rpartition(".")[2])
-    if py_minor >= 13:  # noqa: PLR2004
-        session.env.update(
-            MULTIDICT_NO_EXTENSIONS="1",
-        )
     session.install(".[examples]", "-r", "requirements/test-requirements.txt")
 
     known_deprecations = [
